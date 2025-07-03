a2af7860fcfad117ec11774ddb886829c863e62d
HajiraImran <hajiraimran900@gmail.com>
2025-07-03 10:27:43 +0500

    updated files

--- Services/UserStateService.cs
+++ Services/UserStateService.cs
@@ -1,17 +1,14 @@
-﻿namespace FFCProject.Services
+﻿public class UserStateService
 {
-    public class UserStateService
+    public string Email { get; private set; }
+
+    public void SetEmail(string email)
     {
-        public string Email { get; private set; }
+        Email = email;
+    }
 
-        public void SetEmail(string email)
-        {
-            Email = email;
-        }
-
-        public void Clear()
-        {
-            Email = null;
-        }
+    public void Clear()
+    {
+        Email = null;
     }
 }
